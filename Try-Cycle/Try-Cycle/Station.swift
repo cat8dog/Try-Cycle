99ef221a5b47d5039357915bd0cf915d28a306fe
cat8dog <catherine.reyto@gmail.com>
2015-09-09 14:58:53 -0400

    Stage 1 completed

---
+++ Try-Cycle/Try-Cycle/Station.swift
@@ -0,0 +1,48 @@
+
+
+import Foundation
+
+struct Station {
+    
+    var availableBikes:Int?
+    var availableDocks:Int?
+    var latitude:Float?
+    var longitude:Float?
+    var stationName:String?
+    
+    init(json:NSDictionary) {
+        if let bikeShareStations = json["stationBeanList"] as? NSArray {
+            var bikeShareDepots = bikeShareStations
+            for var i = 0; i < bikeShareDepots.count; i++ {
+                
+                
+                var bikeShareData = bikeShareDepots[i] as? NSDictionary
+                if let bikeShare = bikeShareData {
+                    if let bike = bikeShare["availableBikes"] as? Int {
+                        self.availableBikes = bike as Int
+                        println(self.availableBikes!)
+                    }
+                    
+                    if let dock = bikeShare["availableDocks"] as? Int {
+                        self.availableDocks = dock as Int
+                        println(self.availableDocks!)
+                    }
+                    
+                    if let lat = bikeShare["latitude"] as? Float {
+                        self.latitude = lat as Float
+                        println(self.latitude!)
+                    }
+                    
+                    if let long = bikeShare["longitude"] as? Float {
+                        self.longitude = long as Float
+                        println(self.longitude!)
+                    }
+                    if let station = bikeShare["stationName"] as? String {
+                        self.stationName = station as String
+                        println(self.stationName!)
+                    }
+                }
+            }
+        }
+    }
+}
--- Try-Cycle/Try-Cycle/ViewController.swift
+++ Try-Cycle/Try-Cycle/ViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Adding external web-service URL to the application as an NSURL object:
+        var endpoint = NSURL(string: "http://www.bikesharetoronto.com/stations/json")
+        
+        // creating a data object that will return the formatted JSON, and pass the URL to it:
+        var data = NSData(contentsOfURL: endpoint!)
+        
+        // printing "data" will return the raw data object.  Needs to be formatted into JSON.
+        print(data)
+        
+        
+        // The NSJSONSerialization class will handle the data object and create the serialzed JSON object (for later use)
+        if let json = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSDictionary {
+            
+            var station = Station(json: json)
+            
+            // will return readable data that can be used to start plotting map.
+            print(json)
+            
+            
+            // Since current data is an array of dictionaries, we instruct the compiler to return an arrary of dictionaries.  Isolate one station at a time by specifying that within this dictionary, we have keys for available bikes, available docks, the latitude and longitude and station name.  
+            if let bikeShareStations = json["stationBeanList"] as? NSArray {
+                
+                var bikeShareData = bikeShareStations[0] as? NSDictionary
+            
+                if let bikeShare = bikeShareData {
+                    println(bikeShare["availableBikes"]!)
+                    println(bikeShare["availableDocks"]!)
+                    println(bikeShare["latitude"]!)
+                    println(bikeShare["longitude"]!)
+                    println(bikeShare["stationName"]!)
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
